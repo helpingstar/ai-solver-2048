44435d99872be55c201828da99bcb2e5a8c175d2
helpingstar <iamhelpingstar@gmail.com>
2026-03-22 16:07:39 +0900

    feat(nav): react to root nav state changes

--- app/src/main/java/io/github/helpigstar/aisolver2048/ui/platform/feature/rootnav/RootNavScreen.kt
+++ app/src/main/java/io/github/helpigstar/aisolver2048/ui/platform/feature/rootnav/RootNavScreen.kt
@@ -1,15 +1,19 @@
 package io.github.helpigstar.aisolver2048.ui.platform.feature.rootnav
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.navOptions
+import io.github.helpigstar.aisolver2048.ui.onboarding.feature.navigateToWelcome
 import io.github.helpigstar.aisolver2048.ui.onboarding.feature.welcomeDestination
 import io.github.helpigstar.aisolver2048.ui.platform.components.util.rememberAisolver2048NavController
 import io.github.helpigstar.aisolver2048.ui.platform.feature.splash.SplashRoute
 import io.github.helpigstar.aisolver2048.ui.platform.feature.splash.splashDestination
+import io.github.helpigstar.aisolver2048.ui.workspace.feature.workspace.navigateToWorkspace
 import io.github.helpigstar.aisolver2048.ui.workspace.feature.workspace.workspaceDestination
 
 @Composable
@@ -20,6 +24,13 @@ fun RootNavScreen(
 ) {
     val state by viewModel.stateFlow.collectAsStateWithLifecycle()
 
+    val isNotSplashScreen = state != RootNavState.Splash
+    LaunchedEffect(isNotSplashScreen) {
+        if (isNotSplashScreen) {
+            onSplashScreenRemoved()
+        }
+    }
+
     NavHost(
         navController = navController,
         startDestination = SplashRoute
@@ -27,5 +38,28 @@ fun RootNavScreen(
         splashDestination()
         welcomeDestination()
         workspaceDestination()
+    }
+
+    val rootNavOptions = navOptions {
+        popUpTo(navController.graph.id) {
+            inclusive = false
+            saveState = false
+        }
+        launchSingleTop = true
+        restoreState = false
+    }
+
+    LaunchedEffect(state) {
+        when (state) {
+            RootNavState.Splash -> Unit
+
+            RootNavState.Onboarding -> {
+                navController.navigateToWelcome(navOptions = rootNavOptions)
+            }
+
+            RootNavState.Workspace -> {
+                navController.navigateToWorkspace(navOptions = rootNavOptions)
+            }
+        }
     }
 }
