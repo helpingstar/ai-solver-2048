096131f6ded4302e480dde5bbf65831d8aba67ff
helpingstar <iamhelpingstar@gmail.com>
2026-04-18 08:53:59 +0900

    build: set target sdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ configure<ApplicationExtension> {
         targetSdk {
             version = release(libs.versions.targetSdk.get().toInt())
         }
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
